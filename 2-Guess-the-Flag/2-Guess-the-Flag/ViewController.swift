3b2a77cec0cebc3fab885074472bba9bace71e21
audreywelch <welch.audreyl@gmail.com>
2019-09-16 12:14:31 -0700

    Project 2 added and UI setup complete

---
+++ 2-Guess-the-Flag/2-Guess-the-Flag/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  2-Guess-the-Flag
+//
+//  Created by Audrey Welch on 9/16/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
