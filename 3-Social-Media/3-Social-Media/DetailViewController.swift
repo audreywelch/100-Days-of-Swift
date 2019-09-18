1645790a5f235dac01df491fa5f82012454c307d
audreywelch <welch.audreyl@gmail.com>
2019-09-18 12:46:07 -0700

    Added UIActivityViewController for sharing capabilities

--- 3-Social-Media/3-Social-Media/DetailViewController.swift
+++ 3-Social-Media/3-Social-Media/DetailViewController.swift
@@ -21,6 +21,9 @@ class DetailViewController: UIViewController {
         
         title = "Picture \(selectedPictureNumber) of \(totalPictures)"
         
+        // Create bar button item and trigger shareTapped() method when tapped
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         // Large titles only in the first view controller
         navigationItem.largeTitleDisplayMode = .never
 
@@ -42,6 +45,25 @@ class DetailViewController: UIViewController {
         
         // Only hide the navigation bar on tapping in the DetailView
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    // Allow sending photos via AirDrop, Twitter, etc.
+    @objc func shareTapped() {
+        
+        // In case the image view does not have an image inside, convert to JPEG data
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        // Create a UIActivityViewController (iOS method of sharing content w/ other apps)
+        // Pass in the array of items you want to share and an array of any of my app's services to make sure are on the list
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        
+        // Tell iOS where the activity view controller should appear from, anchor it from the right bar button item
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(vc, animated: true)
     }
     
 
