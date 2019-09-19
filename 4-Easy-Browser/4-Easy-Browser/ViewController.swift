0d281d36ca2f3976af910c2f2999ab9699c3a5a2
audreywelch <welch.audreyl@gmail.com>
2019-09-19 10:22:31 -0700

    Added WKWebView

--- 4-Easy-Browser/4-Easy-Browser/ViewController.swift
+++ 4-Easy-Browser/4-Easy-Browser/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        
+        // Create a new instance of WKWebView web browser and assign to webView property
+        webView = WKWebView()
+        
+        // Set webView's delegate to the current view controller
+        webView.navigationDelegate = self
+        
+        // Make the root view of the view controller the webView
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create a URL
+        let url = URL(string: "https://lambdaschool.com/lambda-students/audrey-welch")!
+        
+        // Create a new URLRequest object from the URL and give it to the webView to laod
+        webView.load(URLRequest(url: url))
+        
+        // Enable a property on web view that allows users to swipe from left or right edge to move backward or forward in their web browsing
+        webView.allowsBackForwardNavigationGestures = true
+        
     }
 
 
