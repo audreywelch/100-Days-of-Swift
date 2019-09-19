a4cfb3fb4c0d8fe38e820d4b3a01e4c959135f54
audreywelch <welch.audreyl@gmail.com>
2019-09-19 10:11:12 -0700

    Project 4 created

---
+++ 4-Easy-Browser/4-Easy-Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  4-Easy-Browser
+//
+//  Created by Audrey Welch on 9/19/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
