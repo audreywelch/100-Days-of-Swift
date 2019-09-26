39cdf73c2ace985098744509c37449c6c6c632de
audreywelch <welch.audreyl@gmail.com>
2019-09-26 15:15:14 -0700

    Accounted for correct error messages to alert users of mistakes

--- 5-Word-Scramble/5-Word-Scramble/View Controllers/ViewController.swift
+++ 5-Word-Scramble/5-Word-Scramble/View Controllers/ViewController.swift
@@ -75,6 +75,9 @@ class TableViewController: UITableViewController {
         
         let lowerAnswer = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
@@ -82,9 +85,25 @@ class TableViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)."
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
         
     }
     
