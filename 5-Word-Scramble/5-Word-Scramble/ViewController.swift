5f1c0be1ac27f0ca659816ca977434d18013408d
audreywelch <welch.audreyl@gmail.com>
2019-09-26 12:12:19 -0700

    Created initial project 5

---
+++ 5-Word-Scramble/5-Word-Scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  5-Word-Scramble
+//
+//  Created by Audrey Welch on 9/26/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
