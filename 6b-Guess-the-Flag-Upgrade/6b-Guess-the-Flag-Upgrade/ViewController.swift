ee86da28c96504e54f60195095c16a1db8a76d17
audreywelch <welch.audreyl@gmail.com>
2019-10-01 12:28:37 -0700

    Used Visual Format Language to create and arrange 5 labels

---
+++ 6b-Guess-the-Flag-Upgrade/6b-Guess-the-Flag-Upgrade/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  6b-Guess-the-Flag-Upgrade
+//
+//  Created by Audrey Welch on 10/1/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+        
+        // Visual Format Language
+        
+        // Add an array of constraints to the view
+        // Convert VFL into an array of constraints
+        // "H:|[label] >> horizontally, make the label go edge to edge in the view
+        // H = horizontal
+        // V = vertical
+        // | = edge
+        // - = space
+        for label in viewsDictionary.keys {
+            
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+            
+            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        }
+        
+    }
+
+
+}
+
