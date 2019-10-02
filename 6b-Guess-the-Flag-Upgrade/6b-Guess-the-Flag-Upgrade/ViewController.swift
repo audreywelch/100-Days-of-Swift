30d14a541446d54a9a046bd99470269b7554ed4c
audreywelch <welch.audreyl@gmail.com>
2019-10-02 16:24:06 -0700

    Created challenge project and added alert controller to collect list items

--- 6b-Guess-the-Flag-Upgrade/6b-Guess-the-Flag-Upgrade/ViewController.swift
+++ 6b-Guess-the-Flag-Upgrade/6b-Guess-the-Flag-Upgrade/ViewController.swift
@@ -51,7 +51,7 @@ class ViewController: UIViewController {
         
         let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
         
-        // Visual Format Language
+        // MARK: - Visual Format Language
         
         // Add an array of constraints to the view
         // Convert VFL into an array of constraints
@@ -72,15 +72,22 @@ class ViewController: UIViewController {
 //            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
 //        }
         
+        // MARK: - Using Anchors
+        
         var previous: UILabel?
         
+        
+        
         for label in [label1, label2, label3, label4, label5] {
+            
+            
             label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
             label.heightAnchor.constraint(equalToConstant: 88).isActive = true
             
             if let previous = previous {
                 // We have a previous label - create a height constraint
                 label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+                
             } else {
                 // This is the first label
                 label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
---
+++ Challenge-2/Challenge-2/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Challenge-2
+//
+//  Created by Audrey Welch on 10/2/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListTableViewController: UITableViewController {
+    
+    var shoppingList: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add right bar button item
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTextField))
+        
+        // Add left bar button item
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(clearShoppingList))
+    }
+    
+    @objc func addTextField() {
+        
+        let ac = UIAlertController(title: "Enter Item to Add to List", message: nil, preferredStyle: .alert)
+        
+        // Add a single text field
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let newItem = ac?.textFields?[0].text else { return }
+            self?.submit(newItem)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+        
+    }
+    
+    @objc func clearShoppingList() {
+        
+    }
+    
+    func submit(_ newItem: String) {
+        shoppingList.insert(newItem, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+
+
+}
+
