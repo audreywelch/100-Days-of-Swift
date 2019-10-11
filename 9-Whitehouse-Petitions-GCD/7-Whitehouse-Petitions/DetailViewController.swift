0ed5f690620e7a9331d4cf24c3438f87ecb6c148
audreywelch <welch.audreyl@gmail.com>
2019-10-11 09:49:11 -0700

    Project 9 created

---
+++ 9-Whitehouse-Petitions-GCD/7-Whitehouse-Petitions/DetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.swift
+//  7-Whitehouse-Petitions
+//
+//  Created by Audrey Welch on 10/3/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+    var webView: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        webView.loadHTMLString(html, baseURL: nil)
+    }
+
+}
---
+++ 9-Whitehouse-Petitions-GCD/7-Whitehouse-Petitions/ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  7-Whitehouse-Petitions
+//
+//  Created by Audrey Welch on 10/3/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var petitions: [Petition] = []
+    var filteredPetitions: [Petition] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Whitehouse Petitions"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterResults))
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            // backup: "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        // Make sure the URL is valid
+        if let url = URL(string: urlString) {
+            // Create a new Data object using its contentsOf method to return the content from the URL
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+        
+        showError()
+    }
+    
+    @objc func showCredits() {
+        
+        let ac = UIAlertController(title: "Credits", message: "The information in this application is provided by the We The People API of the Whitehouse.", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .cancel)
+        
+        ac.addAction(action)
+        present(ac, animated: true)
+    }
+    
+    @objc func filterResults() {
+        
+        let ac = UIAlertController(title: "Filter Results", message: "Enter keywords of petitions you'd like to see.", preferredStyle: .alert)
+        
+        // Add a single text field to the controller
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            // Read the value that was inserted into the text field
+            guard let keywords = ac?.textFields?[0].text else { return }
+            self?.submit(keywords)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ keywords: String) {
+        
+        filteredPetitions = []
+        
+        let lowerKeyword = keywords.lowercased()
+        
+        for eachPetition in petitions {
+            if eachPetition.title.lowercased().contains(lowerKeyword) || eachPetition.body.lowercased().contains(lowerKeyword) {
+                filteredPetitions.append(eachPetition)
+            }
+        }
+        
+        tableView.reloadData()
+        
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filteredPetitions = petitions
+            tableView.reloadData()
+        }
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPetitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = filteredPetitions[indexPath.row]
+        
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        
+        return cell
+    }
+    
+    // Load the DetailViewController class directly
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = filteredPetitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+
+}
+
