dd0e8812c33e335542690189f337a2cb05f9a54b
audreywelch <welch.audreyl@gmail.com>
2019-11-08 10:38:52 -0800

    Added UI for project 10

---
+++ 10-Names-to-Faces/10-Names-to-Faces/PersonCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCollectionViewCell.swift
+//  10-Names-to-Faces
+//
+//  Created by Audrey Welch on 11/8/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class PersonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ 10-Names-to-Faces/10-Names-to-Faces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10-Names-to-Faces
+//
+//  Created by Audrey Welch on 11/8/19.
+//  Copyright © 2019 Audrey Welch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
