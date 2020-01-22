892d4ac4d6e1b04cc9902c4fe731c0832b8f32dc
audreywelch <welch.audreyl@gmail.com>
2020-01-22 13:09:06 -0800

    Allowed for saving to the iOS photo library

--- 13-Instafilter/13-Instafilter/ViewController.swift
+++ 13-Instafilter/13-Instafilter/ViewController.swift
@@ -74,6 +74,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBAction func save(_ sender: Any) {
         
+        guard let image = imageView.image else { return }
+
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
@@ -117,6 +120,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             // Assign that UIImage to the image view
             imageView.image = processedImage
+        }
+        
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        
+        if let error = error {
+            
+            // If we got back an error
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
         }
         
     }
